55f96671b9bf5dafd787794c3d0b6781a20c36cc
ludwings <ludwings0330@gmail.com>
2021-11-18 18:56:30 +0900

    게시판 목록 보기 기능 추가

--- src/main/java/com/ludwings/book/springboot/service/PostsService.java
+++ src/main/java/com/ludwings/book/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.ludwings.book.springboot.service;
 
 import com.ludwings.book.springboot.domain.posts.Posts;
 import com.ludwings.book.springboot.domain.posts.PostsRepository;
+import com.ludwings.book.springboot.web.dto.PostsListResponseDto;
 import com.ludwings.book.springboot.web.dto.PostsResponseDto;
 import com.ludwings.book.springboot.web.dto.PostsSaveRequestDto;
 import com.ludwings.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,18 +10,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class PostsService {
 
     private final PostsRepository postsRepository;
 
-    @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
-    @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
@@ -36,4 +40,19 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
 }
